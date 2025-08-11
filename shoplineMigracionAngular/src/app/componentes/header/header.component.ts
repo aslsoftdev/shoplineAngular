afb13adc9e9c077adb71313102fa2e8ec1f2e2c2
ame133 <ame1117c@gmail.com>
2025-08-11 12:25:35 -0600

    build scripts

--- shoplineMigracionAngular/src/app/componentes/header/header.component.ts
+++ shoplineMigracionAngular/src/app/componentes/header/header.component.ts
@@ -9,5 +9,18 @@ import { RouterModule } from '@angular/router';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+   private scripts = [
+    'assets/js/Btj8gIDAfUbg.js',
+    'assets/js/0RYcmtHNzeKD.js',
+    'assets/js/BoX8K9uAwsEZ.js',
+    'assets/js/iPFiVDlbzn6i.js',
+    'assets/js/8eyAMlnnEWX0.js',
+    'assets/js/aS5NYHozGJqT.js',
+    'assets/js/8eyAMlnnEWX0.js',
+    'assets/js/iVNXgkwU9ou3.js',
+    'assets/js/S2wkihMsUG7v.js',
+    'assets/js/YJMB0jXMbwYM.js',
+    'assets/js/VOAGbPBNMqrK.js',
+    'assets/js/a3bN4gMpiVhO.js'
+  ];
 }
